42268eabb467a9ab110c4292c6fc85f5d772357e
Rafael Alves <faelalvesoliveira06@gmail.com>
2025-07-22 22:58:42 -0300

    Usando o context para enviar dados para dentro dos templates do Django.

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,30 @@
 from django.shortcuts import render
 
 
-def exemplo(request):
-    print('exemplo')
-    return render(
-        request,
-        'blog/exemplo.html',
-    )
-
-
 def blog(request):
     print('blog')
+
+    context = {
+        'text': 'Olá Blog'
+    }
+
     return render(
         request,
         'blog/index.html',
+        context,
+    )
+
+
+def exemplo(request):
+    print('exemplo')
+
+    context = {
+        'text': 'Olá Exemplo',
+        'title': 'Essa é uma página de exemplo',
+    }
+
+    return render(
+        request,
+        'blog/exemplo.html',
+        context,
     )
