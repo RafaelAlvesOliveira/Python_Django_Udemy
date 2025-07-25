3f88d1146b34c24d675d5e61f1039e7e5f663c0a
Rafael Alves <faelalvesoliveira06@gmail.com>
2025-07-24 23:32:30 -0300

    Criando urls dinâmicas no Django URL Dispatcher, view e template.

--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,21 @@ def blog(request):
     )
 
 
+def post(request, id):
+    print('post', id)
+
+    context = {
+        # 'text': 'Olá Blog',
+        'posts': posts
+    }
+
+    return render(
+        request,
+        'blog/index.html',
+        context,
+    )
+
+
 def exemplo(request):
     print('exemplo')
 
